2569c57991f9b94a7db46505d4ea49322ee10a0b
Daniel Gomez <daniels26@gmail.com>
2019-10-26 15:14:36 +0200

    Generate player functions

--- player/src/main.py
+++ player/src/main.py
@@ -8,5 +8,32 @@ g = (0,255,0) #opponent players
 b = (0,0,0) #blank
 w = (255,255,255) #current player
 
+
+def connect_to_mqtt():
+    # Connect to MQTT and setup hooks
+    return
+
+def identify_user():
+    # Generate UserId
+    return
+
+def send_direction(uid, newDirection):
+    # Send new direction to MQTT
+    return
+
+def on_message_received(message):
+    # Code to parse the message received from MQTT (extract information and call on_map_received)
+    return
+
+def on_map_received(userId, newDirection):
+    # Update map and publish
+    return
+
+def display_map(map):
+    # Display map
+    return
+
+
 while True:
     sense.show_message("Hello player!")
+
