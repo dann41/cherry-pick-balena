a3fcb3405c9ee845b042313381ceb13e1aacda2e
kani85 <cani.vincenzo@gmail.com>
2019-10-26 14:55:45 +0200

    Update main.py

--- player/src/main.py
+++ player/src/main.py
@@ -2,5 +2,11 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
+#Set color values
+r = (255,0,0) #cherry
+g = (0,255,0) #opponent players
+b = (0,0,0) #blank
+w = (255,255,255) #current player
+
 while True:
     sense.show_message("Hello player!")
