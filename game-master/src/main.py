2c01e1e8edf505f6da1b8ffde124ed0b20e4f09f
kani85 <cani.vincenzo@gmail.com>
2019-10-26 14:46:06 +0200

    Update main.py
    adding check_wall method

--- game-master/src/main.py
+++ game-master/src/main.py
@@ -2,5 +2,18 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
+def check_wall(x, y):
+    new_x = x
+    new_y = y
+    if y == 8:
+        new_y = 7
+    elif y == -1:
+        new_y = 0
+    if x == 8:
+        new_x = 7
+    elif x == -1:
+        new_x = 0
+    return new_x, new_y
+
 while True:
     sense.show_message("Hello game master!")
